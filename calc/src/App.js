fca1fa34348992b4dee5c40bc825d4c28f035992
luka-danger <lukathevegan@gmail.com>
2023-12-27 16:03:56 -0700

    Add more css

--- calc/src/App.js
+++ calc/src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className='calculator-grid'>
         <div className='output'>
-            <div className='previous-operand'></div>
-            <div className='current-operand'></div>
+            <div className='previous-operand'>200</div>
+            <div className='current-operand'>150</div>
         </div>
         <button className='span-two'>AC</button>
         <button>DEL</button>
@@ -21,6 +21,7 @@ function App() {
         <button>7</button>
         <button>8</button>
         <button>9</button>
+        <button>.</button>
         <button>-</button>
         <button>0</button>
         <button className='span-two'>=</button>
