c33d99d2ce2889aaa0961485d4812d5a62cb528f
luka-danger <lukathevegan@gmail.com>
2023-12-27 17:30:38 -0700

    Create Operation Button

--- calc/src/App.js
+++ calc/src/App.js
@@ -1,6 +1,8 @@
 import { useReducer } from 'react';
 import DigitButton from './digitbutton';
+import OperationButton from './operationbutton';
 import './style.css'
+
 
 export const ACTIONS = {
   ADD_DIGIT: 'add-digit', 
@@ -33,19 +35,19 @@ function App() {
         </div>
         <button className='span-two'>AC</button>
         <button>DEL</button>
-        <DigitButton digit='/' dispatch={dispatch} />
+        <OperationButton operation='/' dispatch={dispatch} />
         <DigitButton digit='1' dispatch={dispatch} />
         <DigitButton digit='2' dispatch={dispatch} />
         <DigitButton digit='3' dispatch={dispatch} />
-        <button>x</button>
+        <OperationButton operation='x' dispatch={dispatch} />
         <DigitButton digit='4' dispatch={dispatch} />
         <DigitButton digit='5' dispatch={dispatch} />
         <DigitButton digit='6' dispatch={dispatch} />
-        <button>+</button>
+        <OperationButton operation='+' dispatch={dispatch} />
         <DigitButton digit='7' dispatch={dispatch} />
         <DigitButton digit='8' dispatch={dispatch} />
         <DigitButton digit='9' dispatch={dispatch} />
-        <button>-</button>
+        <OperationButton operation='-' dispatch={dispatch} />
         <DigitButton digit='.' dispatch={dispatch} />
         <DigitButton digit='0' dispatch={dispatch} />
         <button className='span-two'>=</button>
