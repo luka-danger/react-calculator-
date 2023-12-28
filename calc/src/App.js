9e85c61b1c719c9cbbe244f1a5c33f1d51ea013a
luka-danger <lukathevegan@gmail.com>
2023-12-27 18:15:09 -0700

    Add additional cases
    Choose Operation and Clear ACTIONS

--- calc/src/App.js
+++ calc/src/App.js
@@ -25,6 +25,21 @@ function reducer(state, {type, payload}) {
         ...state, 
         currentOperand: `${state.currentOperand || ''}${payload.digit}`
       }
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        }
+      }
+    case ACTIONS.CLEAR:
+      return{}
   }
 }
 
